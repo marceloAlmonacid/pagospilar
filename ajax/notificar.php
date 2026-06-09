cbea27f02c6932300d7e021f695e3982abebe7c9
mdalmonacid <mdalmonacid@invap.com.ar>
2026-06-09 16:00:29 -0300

    fix: remover enlace de mercadopago generado por alias ya que daba error de link no disponible

--- ajax/notificar.php
+++ ajax/notificar.php
@@ -97,8 +97,7 @@ if($_POST){
                         $mensaje .= "⚠️ *Aumentos en lo que pagas:*\n- " . implode("\n- ", $u['incrementos_individuales']) . "\n\n";
                     }
                     
-                    $mensaje .= "Alias para transferir: *gira.barro*\n";
-                    $mensaje .= "Abrir Mercado Pago:\nhttps://link.mercadopago.com.ar/girabarro\n\n";
+                    $mensaje .= "Alias para transferir: *gira.barro*\n\n";
                     $mensaje .= "Gracias! ❤️\n\n_Mensaje generado por la App Pagos Pilar_\nhttps://pagospilar.dpdns.org";
 
                     if(enviarWhatsApp($u['telefono'], $mensaje)){
@@ -209,8 +208,7 @@ if($_POST){
                     $mensaje .= "⚠️ *Aumentos en lo que pagas:*\n- " . implode("\n- ", $incrementos_individuales) . "\n\n";
                 }
 
-                $mensaje .= "Alias para transferir: *gira.barro*\n";
-                $mensaje .= "Abrir Mercado Pago:\nhttps://link.mercadopago.com.ar/girabarro\n\n";
+                $mensaje .= "Alias para transferir: *gira.barro*\n\n";
                 $mensaje .= "Gracias! ❤️\n\n_Mensaje generado por la App Pagos Pilar_\nhttps://pagospilar.dpdns.org";
                 
                 $enviado = false;
